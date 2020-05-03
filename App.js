7720728d9cf6b1af4c66c64291754839c762d8eb
Aditya Kumar Jha <adityajha126@gmail.com>
2020-05-03 16:24:10 +0530

    vivek added

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+  ScrollView,
+  View,
+  Text
+} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <ScrollView>
+        <View>
+          <Text>
+            Edit App.js to change this
+            screen and then come back to see your edits.
+          </Text>
+        </View>
+      </ScrollView>
+    </>
+  );
+};
+
+export default App;
