2f8c1d4cbed355e3fca861002126421499ae0382
vivekchauhan12000 <49834398+vivekchauhan12000@users.noreply.github.com>
2020-05-09 16:30:35 +0530

    commit_playpage

---
+++ src/pages/home/tabTwo.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { StyleSheet,  View ,Text } from 'react-native'
+import Card from "../in-component/card_playtab"
+import { Container , Content } from 'native-base'
+ const tabTwo = ()=> {
+  return (
+    <Container style={styles.container}>
+     <Content>
+   <Card/>
+   <Card/>
+   <Card/>
+   </Content>
+   </Container>
+  )
+}
+
+export default tabTwo
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 0,
+    backgroundColor: "#ebf0f7"
+  },
+  contentList: {
+    flex: 1,
+  }
+})
